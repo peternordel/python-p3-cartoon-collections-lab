2ffd075aa87214d6a1e390832d4586b8868024d0
Peter Nordel <peternordel@gmail.com>
2023-05-02 12:38:15 -0600

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    i = 1
+    for dwarf in dwarves:
+        print(f'{i}. {dwarf}')
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.title() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for call in words:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheeses = ("cheddar", "gouda", "camembert")
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    return None
